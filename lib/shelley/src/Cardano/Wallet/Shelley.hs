52bedfa03b635cb8e98fdca97f19791cd432be0d
Pawel Jakubas <pawel.jakubas@iohk.io>
2020-11-27 21:37:18 +0100

    add script_gap to SeqState plus support migrations plus add getter for VerificatioKeyPool

--- lib/shelley/src/Cardano/Wallet/Shelley.hs
+++ lib/shelley/src/Cardano/Wallet/Shelley.hs
@@ -377,7 +377,7 @@ serveWallet
                     minimumUTxOvalue pp
                 , defaultHardforkEpoch =
                     hardforkEpochNo pp
-                , defaultMultisigPoolGap = Nothing
+                , defaultScriptPoolGap = Nothing
                 }
             )
             (neverFails "db layer should never forecast into the future"
