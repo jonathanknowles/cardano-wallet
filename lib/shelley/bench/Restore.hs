1d64677f35db42c88ed0d31d0a77837f03a0a882
Pawel Jakubas <pawel.jakubas@iohk.io>
2020-11-27 10:36:52 +0100

    correct behavior of extention of verification key pool
    make db bench compiling
    
    more corrrections

--- lib/shelley/bench/Restore.hs
+++ lib/shelley/bench/Restore.hs
@@ -690,6 +690,7 @@ withBenchDBLayer ti action =
         , Sqlite.defaultDesiredNumberOfPool = 0
         , Sqlite.defaultMinimumUTxOValue = Coin 0
         , Sqlite.defaultHardforkEpoch = Nothing
+        , defaultMultisigPoolGap = Nothing
         }
 
 prepareNode
